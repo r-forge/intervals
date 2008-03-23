d93941c547f790b6be594aac2ed3d7fb31be25f5
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-03-23 13:42:44 +0000

    - Moved one comment to a more logical location.
    - Significantly streamlined code for handling of "Z" intervals.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@18 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/R/interval_union.R
+++ intervals/R/interval_union.R
@@ -11,30 +11,24 @@ ordering_matrix <- matrix(
 
 setGeneric( "interval_union", def = function( x, y, ... ) standardGeneric( "interval_union" ) )
 
-# Note that if integer intervals are forced to double-open notation, then the
-# real-valued code works immediately, with no need for back-conversion
-# afterwards. This is because the open intervals corresponding to two
-# consecutive integers overlap one another.
-
 setMethod(
           "interval_union",
           signature( "Intervals_full", "missing" ),
           function( x ) {
             # TO DO: remove rows with NA values
             # TO DO: remove empty rows in result
-            # TO DO: revise/simplify handling of 'Z' data
-            if ( type(x) == "Z" ) {
-              #  We will work on Z intervals as closed only
-              x[ !closed(x)[,1], 1 ] <- x[ !closed(x)[,1], 1 ] + 1
-              x[ closed(x)[,2], 2 ] <- x[ closed(x)[,2], 2 ] + 1
-              closed(x) <- TRUE
-            }
+            #
+            # Note that if integer intervals are forced to double-open notation,
+            # then the real-valued code works immediately, with no need for
+            # back-conversion afterwards. This is because the open intervals
+            # corresponding to two consecutive integers overlap one another.
+            if ( type(x) == "Z" ) x <- open_intervals(x)
             closed <- as.vector( t( closed(x) ) )
             data <- data.frame(
                                pos = as.vector( t( x@.Data ) ),
                                ordering = ordering_matrix[ cbind( ifelse( closed, 2L, 1L ), rep( c(1L,2L), nrow( x ) ) ) ],
                                closed = closed,
-                               score = rep( c( 1, -1 ), nrow( x ) )                       
+                               score = rep( c( 1, -1 ), nrow( x ) )              
                                )
             data <- data[ with( data, order( pos, ordering ) ), ]
             data$sum <- cumsum( data$score )
@@ -48,10 +42,7 @@ setMethod(
                                closed = cbind( closed[ first ], closed[ z ] ),
                                type = type( x )
                                )
-                           )            
-            if ( type( x ) == "Z" ) {
-              result[,2] <- result[,2] - 1
-            }
+                           )
             colnames( result ) <- colnames( x )
             return( result )
           }
@@ -64,9 +55,9 @@ setMethod(
           function( x ) {
             result <- as( interval_union( as( x, "Intervals_full" ) ), "Intervals" )
             if ( type( x ) == "Z" ) {
-              # We always get, but may not want, double-closed results for type Z
-              if ( !closed(x)[1] ) result[,1] <- result[,1] - 1
-              if ( !closed(x)[2] ) result[,2] <- result[,2] + 1
+              # We always get, but may not want, double-open results for type Z
+              if ( closed(x)[1] ) result[,1] <- result[,1] + 1
+              if ( closed(x)[2] ) result[,2] <- result[,2] - 1
               closed( result ) <- closed( x )
             }
             return( result )
