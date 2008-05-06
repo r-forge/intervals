1df2ec122f7b90fcf28f7b56955afca883fff967
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-05-06 13:58:28 +0000

    - The size methods now have an "as" argument which overrides the "type"   slot. Documentation was updated to show this.
    - Incremented revision number: 1.2.2
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/R/size.R
+++ intervals/R/size.R
@@ -1,13 +1,13 @@
 # For R, size is Lebesgue measure, so closure is irrelevant.
 
-setGeneric( "size", def = function(x) standardGeneric( "size" ) )
+setGeneric( "size", def = function( x, ... ) standardGeneric( "size" ) )
 
 setMethod(
           "size",
           signature( "Intervals" ),
-          function(x) {
+          function( x, as = type(x) ) {
             result <- x[,2] - x[,1]            
-            if ( type(x) == "Z" ) {
+            if ( as == "Z" ) {
               ties <- x[,2] == x[,1]
               result[ ties ] <- ifelse( all( closed(x) ), 1, 0 )
               result[ !ties ] <- result[ !ties ] + sum( closed(x) ) - 1
@@ -19,9 +19,9 @@ setMethod(
 setMethod(
           "size",
           signature( "Intervals_full" ),
-          function(x) {
+          function( x, as = type(x) ) {
             result <- x[,2] - x[,1]            
-            if ( type(x) == "Z" ) {
+            if ( as == "Z" ) {
               ties <- x[,2] == x[,1]
               result[ ties ] <- ifelse( closed(x)[,1] & closed(x)[,2], 1, 0 )
               result[ !ties ] <- result[ !ties ] + rowSums( closed(x) )[ !ties ] - 1
