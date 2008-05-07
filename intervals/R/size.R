de72fc320fb988e3da31c8be494f7f4db57402a1
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-05-07 10:19:51 +0000

    - Added proper NA handling to size methods.
    - Fixed bugs in size methods for Intervals_full class, and also streamlined the
      code a bit.
    
    - Incremented revision number: 1.2.3
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/R/size.R
+++ intervals/R/size.R
@@ -1,8 +1,5 @@
 # For R, size is Lebesgue measure, so closure is irrelevant.
 
-# TO DO: this currently crashes, due to the "ties" logical vector when there are
-# NAs in the endpoint matrix.
-
 setGeneric( "size", def = function( x, ... ) standardGeneric( "size" ) )
 
 setMethod(
@@ -12,8 +9,9 @@ setMethod(
             result <- x[,2] - x[,1]            
             if ( as == "Z" ) {
               ties <- x[,2] == x[,1]
+              ties[ is.na( ties ) ] <- FALSE
               result[ ties ] <- ifelse( all( closed(x) ), 1, 0 )
-              result[ !ties ] <- result[ !ties ] + sum( closed(x) ) - 1
+              result[ !ties ] <- result[ !ties ] + sum( closed(x) ) - 1 # NAs just stay NA
             }
             return( result )
           }
@@ -26,8 +24,10 @@ setMethod(
             result <- x[,2] - x[,1]            
             if ( as == "Z" ) {
               ties <- x[,2] == x[,1]
-              result[ ties ] <- ifelse( closed(x)[,1] & closed(x)[,2], 1, 0 )
-              result[ !ties ] <- result[ !ties ] + rowSums( closed(x) )[ !ties ] - 1
+              ties[ is.na( ties ) ] <- FALSE
+              rs <- rowSums( closed(x) )
+              result[ ties ] <- ifelse( rs[ ties ] == 2, 1, 0 )
+              result[ !ties ] <- result[ !ties ] + rs[ !ties ] - 1
             }
             return( result )
           }
