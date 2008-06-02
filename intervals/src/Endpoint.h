3073fa0beb8dcc2cbdc334cb993fa03b837815d3
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-06-02 21:25:21 +0000

    - Switched over to an enum approach, which is much more readable.
    - Got the < operator working.
    
    - Added a print method.
    
    - Changed index data type: the old int type was likely too small for many real
      applications. 
    
    - Added a default tol value matching what all.equal() uses on my Macbook.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@84 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/src/Endpoint.h
+++ intervals/src/Endpoint.h
@@ -1,54 +1,94 @@
+#include <algorithm> // For max
+
+
+
+//////// enums
+
+enum type_enum { query, target };
+enum side_enum { left, right };
+enum closure_enum { closed, open };
+
+
+
+
+//////// Class definition
+
 class Endpoint{
   
  private:
   
-  double x;
-  bool closed, left, query;
-  int index;
+  double pos;
+  type_enum type;
+  side_enum side;
+  closure_enum closure;
+  // bool closed, left, query;
+  unsigned long index;
   
  public:
 
   static double tol;
+  static int state_array[2][2][2];
   
   Endpoint() {}
-  Endpoint(double x, bool c, bool l, bool q, int i) { 
-    x = x; closed = c; left = l; query = q, index = i; 
+  Endpoint(double p, type_enum t, side_enum s, closure_enum c, unsigned long i) { 
+    pos = p; type = t; side = s; closure = c; index = i; 
   }
 
-  int score() const { 
-    /*
-      Let Q/T be query/target, L/R be left/right, and O/C be open/closed. Our
-      ordering, when x is effectively tied, is then:
-
-        QRO < TRO ... < TLC < QLC < QRC < TRC ... < TLO < QLO
-
-      The basic principals are, for similar closure, start targets before
-      queries but finish them after queries. For abutting intervals, start new
-      intervals before finishing old ones, unless one or both endpoints are
-      open, in which case we should finish old intervals first.
-    */
-    return( 
-	   query ?
-	   // Query
-	   ( left ? 
-	     ( closed ? 3 : 7 ) : // Left
-	     ( closed ? 4 : 0 )   // Right
-	     ) :
-	   // Target
-	   ( left ? 
-	     ( closed ? 2 : 6 ) : // Left
-	     ( closed ? 5 : 1 )   // Right
-	     )
-	    );
-  }
+  int state() const { return( state_array[type][side][closure] ); }
 
   bool operator< (const Endpoint& other) const {
-    // We use relative difference for assessing equality of x. 
-    double rd = ( this->x - other.x ) / std::max( other.x, this->x );
+    // We use relative difference for assessing equality of pos. 
+    // TO DO: what if the max is 0!
+    double rd = ( this->pos - other.pos ) / abs( std::max( other.pos, this->pos ) );
+    std::cout << "rd = " << rd << std::endl;
     if ( rd > tol ) return( false );
     if ( rd < -1. * tol ) return( true );
     // The x values are effectively equal. Assess based on endpoint type flags.
-    return( this->score() < other.score() );
+    return( this->state() < other.state() );
   }
 
+  void print() {
+    std::cout << 
+      "pos = " << pos <<
+      ", " << ( type == query ? "query" : "target" ) <<
+      ", " << ( side == left ? "left" : "right" ) <<
+      ", " << ( closure == closed ? "closed" : "open" ) <<
+      ", index = " << index <<
+      std::endl;
+  }
+
+};
+
+
+
+
+//////// Tolerance for floating point equality 
+
+// This R all.equal default on my Macbook.
+
+double Endpoint::tol = 1.490116e-08;
+
+
+
+
+//////// Ordering for tied endpoints
+
+/*
+  Let Q/T be query/target, L/R be left/right, and O/C be open/closed. Our
+  ordering, when pos is effectively tied, is then:
+
+    QRO < TRO ... < TLC < QLC < QRC < TRC ... < TLO < QLO
+     0     1         2     3     4     5         6     7
+
+  The basic principals are, for similar closure, start targets before
+  queries but finish them after queries. For abutting intervals, start new
+  intervals before finishing old ones, unless one or both endpoints are
+  open, in which case we should finish old intervals first.
+*/
+
+int Endpoint::state_array[2][2][2] = {
+  // Query {{LC, LO}, {RC, RO}}
+  {{3,7},{4,0}},
+  // Target {{LC, LO}, {RC, RO}}
+  {{2,6},{5,1}}
 };
