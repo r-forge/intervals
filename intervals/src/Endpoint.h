1eeb38f49490cdcbb0678a36b6444787aa0460d5
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-06-03 09:46:11 +0000

    - Added a string-based method for looking at Endpoint objects, which will be   more convenient for Rprintf.
    - Added Endpoints class, which derives from vector< Endpoint >.
    
    - Reordered the closure enum so that casting from logicals gives the right
      value. Made required changes elsewhere.
    
    - Fixed several real or potential bugs in operator<. In particular, we now
      handled double zero correctly.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@85 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/src/Endpoint.h
+++ intervals/src/Endpoint.h
@@ -1,60 +1,71 @@
+#include <vector>
 #include <algorithm> // For max
-
+#include <sstream>
 
 
 //////// enums
 
 enum type_enum { query, target };
 enum side_enum { left, right };
-enum closure_enum { closed, open };
+enum closure_enum { open, closed };
 
 
 
 
-//////// Class definition
+//////// index type
+
+typedef unsigned long ind_t;
+
+
+
+
+//////// Endpoint class
 
 class Endpoint{
   
- private:
+private:
   
+  ind_t index;
   double pos;
   type_enum type;
   side_enum side;
   closure_enum closure;
-  // bool closed, left, query;
-  unsigned long index;
-  
- public:
 
-  static double tol;
   static int state_array[2][2][2];
-  
-  Endpoint() {}
-  Endpoint(double p, type_enum t, side_enum s, closure_enum c, unsigned long i) { 
-    pos = p; type = t; side = s; closure = c; index = i; 
-  }
 
   int state() const { return( state_array[type][side][closure] ); }
+  
+public:
+
+  static double tol;
+  
+  Endpoint(ind_t i, double p, type_enum t, side_enum s, closure_enum c) { 
+     index = i; pos = p; type = t; side = s; closure = c; 
+  }
 
   bool operator< (const Endpoint& other) const {
-    // We use relative difference for assessing equality of pos. 
-    // TO DO: what if the max is 0!
-    double rd = ( this->pos - other.pos ) / abs( std::max( other.pos, this->pos ) );
-    std::cout << "rd = " << rd << std::endl;
-    if ( rd > tol ) return( false );
-    if ( rd < -1. * tol ) return( true );
-    // The x values are effectively equal. Assess based on endpoint type flags.
+    // We use relative difference for assessing equality of pos, and compare on
+    // the basis of state in case of (effective) ties.
+    double max_abs, rd;
+    max_abs = std::max( abs( other.pos ), abs( this->pos ) );
+    if ( max_abs > 0. ) {
+      rd = ( this->pos - other.pos ) / max_abs;
+      if ( rd > tol ) return( false );
+      if ( rd < -1. * tol ) return( true );
+    }
     return( this->state() < other.state() );
   }
 
-  void print() {
-    std::cout << 
-      "pos = " << pos <<
-      ", " << ( type == query ? "query" : "target" ) <<
-      ", " << ( side == left ? "left" : "right" ) <<
-      ", " << ( closure == closed ? "closed" : "open" ) <<
-      ", index = " << index <<
-      std::endl;
+  std::string str() const {
+    std::stringstream ss;
+    ss << 
+      "index = " << index <<
+      ", pos = " << pos <<
+      " (" << ( type ? "target" : "query" ) <<
+      ", " << ( side ? "right" : "left" ) <<
+      ", " << ( closure ? "closed" : "open" ) <<
+      ")";
+    return( ss.str() );
   }
 
 };
@@ -62,7 +73,29 @@ class Endpoint{
 
 
 
-//////// Tolerance for floating point equality 
+//////// Endpoints class
+
+class Endpoints : public std::vector< Endpoint > {
+public:
+  Endpoints( const double * pos, const bool * closure, ind_t n, type_enum type, bool is_full ) {
+    /*
+      The pos pointer should point to an nx2 array of endpoints, and the closure
+      pointer, to either an array of booleans of the same size (if full = true)
+      or an array of two booleans (if full = false).
+     */
+    ind_t i;
+    this->reserve( 2 * n );
+    for ( i = 0; i < n; i++ ) {
+      this->push_back( Endpoint( i, pos[i], type, left, (closure_enum) closure[ is_full ? i : 0 ] ) );
+      this->push_back( Endpoint( i, pos[i+n], type, right, (closure_enum) closure[ is_full ? i+n : 1] ) );
+    }
+  }
+};
+
+
+
+
+//////// Default tolerance for floating point equality 
 
 // This R all.equal default on my Macbook.
 
@@ -87,8 +120,8 @@ double Endpoint::tol = 1.490116e-08;
 */
 
 int Endpoint::state_array[2][2][2] = {
-  // Query {{LC, LO}, {RC, RO}}
-  {{3,7},{4,0}},
-  // Target {{LC, LO}, {RC, RO}}
-  {{2,6},{5,1}}
+  // Query {{LO, LC}, {RO, RC}}
+  {{7,3},{0,4}},
+  // Target {{LO, LC}, {RO, RC}}
+  {{6,2},{1,5}}
 };
