047ac9baa915aee367ac8f77257939ae37d8f5f2
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-06-04 14:01:09 +0000

    - Now check that query and target have the same type. Documentation updated on   this point.
    - Proper handling of empty intervals. Examples -- including an example
      contrasting empty and size = 0 behavior for real intervals -- added.
    
    - Example code now uses short-cut object creation techniques: vector endpoints,
      recycling.
    
    - Incremented revision number: 1.4.1.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@98 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/R/interval_overlap.R
+++ intervals/R/interval_overlap.R
@@ -3,9 +3,25 @@ setGeneric( "interval_overlap", def = function( query, target, ... ) standardGen
 setMethod(
           "interval_overlap",
           signature( "Intervals_virtual", "Intervals_virtual" ),
-          function( query, target, tolerance = .Machine$double.eps^0.5, check_valid = TRUE ) {
+          function(
+                   query,
+                   target,
+                   tolerance = .Machine$double.eps^0.5,
+                   check_valid = TRUE
+                   )
+          {
             if ( check_valid && !( validObject(query) && validObject(target) ) )
               stop( "The 'query' and/or 'target' objects are invalid." )
+            if ( type(query) != type(target) )
+              stop( "Both 'query' and 'target' should have the same type." )
+            if ( any( empty( query ) ) ) {
+              warning( "Some empty query intervals encountered. Setting to NA..." )
+              query[ empty(query), ] <- NA
+            }
+            if ( any( empty( target ) ) ) {
+              warning( "Some empty target intervals encountered. Setting to NA..." )
+              target[ empty(target), ] <- NA
+            }
             result <- .Call(
                             "_interval_overlap",
                             query@.Data, target@.Data,
