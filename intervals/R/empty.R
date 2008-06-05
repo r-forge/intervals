48f0a0f09c2724032aebf307f1098173c7b61a81
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-06-05 09:28:05 +0000

    Revised empty methods to use relative difference for checking interval endpoint equality. This is done in R using the rd_equal function, which will not be exported. Rationalized the empty criterion, and updated documentation to reflect both changes. Added a tolerance example to the documentation as well.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@101 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/R/empty.R
+++ intervals/R/empty.R
@@ -1,26 +1,29 @@
-# Intervals with tied endpoints and with two closed endpoint are taken to
-# represent a point.
+rd_equal <- function( x, y, tolerance = .Machine$double.eps^0.5 ) {
+  # Pointwise relative difference computation. RDs less than tolerance are taken
+  # as equality.
+  if ( length(x) != length(y) )
+    stop( "The 'x' and 'y' arguments should have the same length." )
+  max_abs <- pmax( abs(x), abs(y) )
+  result <- rep( FALSE, length(x) )
+  zeros <- max_abs == 0
+  result[ zeros ] <- TRUE
+  result[ !zeros ] <- ( abs( x - y ) / max_abs )[ !zeros ] < tolerance
+}
 
-setGeneric( "empty", def = function(x) standardGeneric( "empty" ) )
+setGeneric( "empty", def = function(x, ...) standardGeneric( "empty" ) )
 
 setMethod(
           "empty",
           signature( "Intervals" ),
-          function(x) {
-            result <- size(x) <= 0
-            if ( type(x) == "R" && all( closed(x) ) )
-              result[ x[,1] == x[,2] ] <- FALSE
-            return( result )
+          function(x, tolerance = .Machine$double.eps^0.5 ) {
+            rd_equal( x[,1], x[,2], tolerance ) & !all( closed(x) )
           }
           )
 
 setMethod(
           "empty",
           signature( "Intervals_full" ),
-          function(x) {
-            result <- size(x) <= 0
-            if ( type(x) == "R" )
-              result[ x[,1] == x[,2] & closed(x)[,1] & closed(x)[,2] ] <- FALSE
-            return( result )
+          function(x, tolerance = .Machine$double.eps^0.5 ) {
+            rd_equal( x[,1], x[,2], tolerance ) & !( closed(x)[,1] & closed(x)[,2] )
           }
           )
