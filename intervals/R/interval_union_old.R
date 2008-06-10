469a82a07396ad33a2ca4a1fe2b6d329c9465d8d
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-06-10 14:29:58 +0000

    The R part of interval_union is now just a wrapper for the C++ code. This old code has temporarily been put in interavl_union_old, and the new code is fully implemented and tested. Documentation was updated to reflect the new tolerance and check_valid arguments as well.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@114 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ intervals/R/interval_union_old.R
@@ -0,0 +1,89 @@
+# Unions are constructed by ordering endpoints. When first and last endpoints
+# coincide, correct ordering -- on the basis of whether they are open or closed,
+# and first or last for their respective interval -- is required for correct
+# results. This matrix establishes the correct ordering, as is used below. It's
+# cute to do this in all R, but we need C for iterval_overlap anyway, so we may
+# eventually want to handle this in C as well.
+
+ordering_matrix <- matrix(
+                          c( 3L, 1L, 2L, 4L ),
+                          nrow = 2,
+                          dimnames = list( c( "open", "closed" ), c( "first", "last" ) )
+                          )
+
+setGeneric( "interval_union_old", def = function( x, ... ) standardGeneric( "interval_union_old" ) )
+
+setMethod(
+          "interval_union_old",
+          signature( "Intervals_full" ),
+          function( x, ... ) {
+            # Note that the combine call will do type checking on x and ...
+            x <- combine( x, ... )
+            # Note that if integer intervals are forced to double-open notation,
+            # then the real-valued code works immediately, with no need for
+            # back-conversion afterwards. This is because the open intervals
+            # corresponding to two consecutive integers overlap one another.
+            has_na <- is.na( x[,1] ) | is.na( x[,2] )
+            if ( any( has_na ) )
+              x <- x[ !has_na, ]
+            if ( nrow(x) == 0 ) result <- x
+            else {
+              if ( type(x) == "Z" ) x <- open_intervals(x)
+              closed <- as.vector( t( closed(x) ) )
+              data <- data.frame(
+                                 pos = as.vector( t( x@.Data ) ),
+                                 ordering = ordering_matrix[ cbind( ifelse( closed, 2L, 1L ), rep( c(1L,2L), nrow( x ) ) ) ],
+                                 closed = closed,
+                                 score = rep( c( 1, -1 ), nrow( x ) )              
+                                 )
+              data <- data[ with( data, order( pos, ordering ) ), ]
+              data$sum <- cumsum( data$score )
+              z <- which( data$sum == 0 )
+              first <- c( 1, z[ z < nrow(data) ] + 1 ) 
+              result <- with(
+                             data,
+                             new(
+                                 "Intervals_full",
+                                 cbind( pos[ first ], pos[ z ] ),
+                                 closed = cbind( closed[ first ], closed[ z ] ),
+                                 type = type( x )
+                                 )
+                             )
+              colnames( result ) <- colnames( x )
+            }
+            return( result[ !empty( result ), ] )
+          }
+          )
+
+setMethod(
+          "interval_union_old",
+          signature( "Intervals" ),
+          # TO DO: make this more efficient (but less clean) by not coercing
+          # Note that the combine call will do type checking on x and ...
+          function( x, ... ) {
+            x <- combine( x, ... )
+            result <- as( interval_union_old( as( x, "Intervals_full" ) ), "Intervals" )
+            if ( type( x ) == "Z" ) {
+              # We always get, but may not want, double-open results for type Z
+              if ( closed(x)[1] ) result[,1] <- result[,1] + 1
+              if ( closed(x)[2] ) result[,2] <- result[,2] - 1
+              closed( result ) <- closed( x )
+            }
+            return( result )
+          }
+          )
+
+setMethod(
+          "interval_union_old",
+          signature( "missing" ),
+          function( x, ... ) {
+            # Permitting do.call use with named lists, since do.call will put
+            # elements whose names are not "x" into the ... argument. Stripping
+            # names, however, puts arguments in place positionally.
+            args <- list(...)
+            names( args ) <- NULL
+            if ( length( args ) == 0 ) return ( NULL )
+            else return( do.call( interval_union_old, args ) )
+          }
+          )
+
