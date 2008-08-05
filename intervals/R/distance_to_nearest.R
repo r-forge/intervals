0f7385ccf9bb14429152cdb52ac84236429ded70
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-08-05 19:27:54 +0000

    - Switched arguments names to more understandable "from" and "to".
    - Added and tested a method for two Intervals_virtual objects.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@125 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/R/distance_to_nearest.R
+++ intervals/R/distance_to_nearest.R
@@ -1,24 +1,39 @@
-setGeneric( "distance_to_nearest", function( x, y ) standardGeneric( "distance_to_nearest" ) )
+setGeneric( "distance_to_nearest", function( from, to ) standardGeneric( "distance_to_nearest" ) )
 
 setMethod(
           "distance_to_nearest",
           signature( "numeric", "Intervals_virtual" ),
-          function( x, y ) {
-            if ( nrow(y) == 0 ) return( rep( as.numeric( NA ), length( x ) ) )
+          function( from, to ) {
+            if ( nrow(to) == 0 ) return( rep( as.numeric( NA ), length( from ) ) )
             # Close, collapse and sort
-            y <- interval_union( if ( type(y) == "Z" ) close_intervals(y) else y )
+            to <- interval_union( if ( type(to) == "Z" ) close_intervals(to) else to )
             # Create interpolating function
-            browser()
-            n <- nrow(y)
-            gap_x <- ( y[ -1, 1 ] + y[ -n, 2 ] ) / 2
-            gap_y <- ( y[ -1, 1 ] - y[ -n, 2 ] ) / 2
-            f <- approxfun( c( y, gap_x ), c( rep( 0, n*2 ), gap_y ) )
+            n <- nrow(to)
+            gap_x <- ( to[ -1, 1 ] + to[ -n, 2 ] ) / 2
+            gap_y <- ( to[ -1, 1 ] - to[ -n, 2 ] ) / 2
+            f <- approxfun( c( to, gap_x ), c( rep( 0, n*2 ), gap_y ) )
             # Compute results
-            below <- x < y[1,1]
-            above <- x > y[n,2]
-            result <- f( x )
-            result[ below ] <- y[1,1] - x[ below ]
-            result[ above ] <- x[ above ] - y[n,2]    
+            below <- from < to[1,1]
+            above <- from > to[n,2]
+            result <- f( from )
+            result[ below ] <- to[1,1] - from[ below ]
+            result[ above ] <- from[ above ] - to[n,2]    
+            names( result ) <- names( from )
+            return( result )
+          }
+          )
+
+setMethod(
+          "distance_to_nearest",
+          signature( "Intervals_virtual", "Intervals_virtual" ),
+          function( from, to ) {
+            overlapped <- sapply( interval_overlap( to, from ), length ) > 0
+            if ( type(from) == "Z" ) from <- close_intervals(from)
+            result <- pmin(
+                           distance_to_nearest( from[,1], to ),
+                           distance_to_nearest( from[,2], to )
+                           )
+            result[ overlapped ] <- 0
             return( result )
           }
           )
