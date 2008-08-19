0f0b0a77b8e4c7c71fe212d255705cc974cbd93a
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-08-19 15:38:07 +0000

    - Adjusted rd_equal() to not use relative difference when one or both values are   infinite.
    - Added handling of NA in one or both vectors.
    
    - Added a new example to the documentation, plus a description of NA and
      infinite value handling.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@140 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/R/rd_equal.R
+++ intervals/R/rd_equal.R
@@ -1,12 +1,15 @@
 rd_equal <- function( x, y, tolerance = .Machine$double.eps^0.5 ) {
   # Pointwise relative difference computation. RDs less than tolerance are taken
-  # as equality.
+  # as equality. When either value is infinite, a simple comparison is used instead.
   if ( length(x) != length(y) )
     stop( "The 'x' and 'y' arguments should have the same length." )
+  has_na <- is.na( x ) | is.na( y )
+  has_inf <- !has_na & ( is.infinite( x ) | is.infinite( y ) )
   max_abs <- pmax( abs(x), abs(y) )
-  result <- rep( FALSE, length(x) )
-  zeros <- max_abs == 0
+  zeros <- !has_na & max_abs == 0
+  result <- ( abs( x - y ) / max_abs ) < tolerance
   result[ zeros ] <- TRUE
-  result[ !zeros ] <- ( abs( x - y ) / max_abs )[ !zeros ] < tolerance
+  result[ has_na ] <- NA
+  result[ has_inf ] <- x[ has_inf ] == y[ has_inf ]
   return( result )
 }
