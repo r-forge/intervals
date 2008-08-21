5e51a319334ce3917e5d3771d81bd1940c307d3f
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-08-21 14:46:08 +0000

    - Completely rewrote empty methods to not use size. In fact, this makes sense:   empty is a property of the set, while size is a property of the measure   applied to the set, and thus varies with measure. In particular, others might   redefine size, but this should not affect empty.
    - Incremented revision number: 0.7.2
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@158 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/R/empty-methods.R
+++ intervals/R/empty-methods.R
@@ -4,10 +4,14 @@ setMethod(
           "empty",
           signature( "Intervals" ),
           function(x, tolerance = .Machine$double.eps^0.5 ) {
-            result <- rd_equal( size(x), rep( 0, nrow(x) ), tolerance )
-            # Valid objects have x[,1] <= x[,2], so...
-            if( type(x) == "R" && all( closed( x ) ) )
-              result[ !is.na( result ) ] <- FALSE
+            result <- rep( FALSE, nrow(x) )
+            result[ is.na( x[,1] ) | is.na( x[,2] ) ] <- NA
+            if ( !all( closed(x) ) ) {
+              # Valid objects have x[,1] <= x[,2], so we only check this case.
+              result[ rd_equal( x[,1], x[,2], tolerance ) ] <- TRUE
+              if ( type(x) == "Z" && !any( closed( x ) ) )
+                result[ rd_equal( x[,1] + 1, x[,2], tolerance ) ] <- TRUE
+            }
             return( result )
           }
           )
@@ -16,9 +20,13 @@ setMethod(
           "empty",
           signature( "Intervals_full" ),
           function(x, tolerance = .Machine$double.eps^0.5 ) {
-            result <- rd_equal( size(x), rep( 0, nrow(x) ), tolerance )
-            if ( type(x) == "R" )
-              result[ !is.na( result ) & closed(x)[,1] & closed(x)[,2] ] <- FALSE
-            return( result )
+            result <- rep( FALSE, nrow(x) )
+            result[ is.na( x[,1] ) | is.na( x[,2] ) ] <- NA
+            any_open <- !( closed(x)[,1] & closed(x)[,2] )
+            both_open <- !closed(x)[,1] & !closed(x)[,2] 
+            result[ any_open & rd_equal( x[,1], x[,2], tolerance ) ] <- TRUE
+            if ( type(x) == "Z" )
+                result[ both_open & rd_equal( x[,1] + 1, x[,2], tolerance ) ] <- TRUE
+            return( result )          
           }
           )
