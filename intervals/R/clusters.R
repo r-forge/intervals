c3a7f013b337af053806254d70d7cfe7bee31599
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-09-23 14:43:05 +0000

    - Re-wrote clusters() to some extent, making methods for both "numeric" and   "Intervals_virtual". Improved the way w was handled to avoid division, which   would be a problem when type is "Z".
    - Added an entry for clusters() to the main package .Rd file.
    
    - Updated documentation and added examples. In particular, I corrected the
      definition of a cluster and included text which justifies behavior when two
      intervals with open endpoints are separated by exactly w.
    
    - Updated NAMESPACE as necessary.
    
    - Incremented revision number: 0.9.7
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@215 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- intervals/R/clusters.R
+++ intervals/R/clusters.R
@@ -1,7 +1,33 @@
-clusters <- function( pos, w, which = FALSE ) {
-  regions <- reduce( Intervals( cbind( pos - w/2, pos + w/2 ), type = "R" ) )
-  clusters <- interval_overlap( pos, regions )
-  clusters <- clusters[ sapply( clusters, length ) > 1 ]
-  if ( which ) return( lapply( clusters, function(i) pos[i] ) )
-  else return( clusters )
-}
+setGeneric( "clusters", function( x, ... ) standardGeneric( "clusters" ) )
+
+setMethod(
+          "clusters",
+          signature( "numeric" ),
+          function( x, w, which = FALSE ) {
+            if ( is.integer(x) ) x <- as.numeric(x)
+            regions <- reduce( Intervals( cbind( x, x + w ), type = "R" ) )
+            clusters <- interval_overlap( x, regions )
+            clusters <- clusters[ sapply( clusters, length ) > 1 ]
+            if ( which ) return( clusters )
+            else return( lapply( clusters, function(i) x[i] ) )
+          }
+          )
+
+setMethod(
+          "clusters",
+          signature( "Intervals_virtual" ),
+          function( x, w, which = FALSE ) {
+            if ( type(x) == "Z" && ( w %% 1 != 0 ) )
+              stop( "Non-integer 'w' supplied for intervals over Z.", call. = FALSE )
+            regions <- reduce(
+                              new(
+                                  class(x),
+                                  cbind( x[,1], x[,2] + w ), closed = closed(x), type = type(x)
+                                  )
+                              )
+            clusters <- interval_overlap( x, regions )
+            clusters <- clusters[ sapply( clusters, length ) > 1 ]
+            if ( which ) return( clusters )
+            else return( lapply( clusters, function(i) x[i,] ) )
+          }
+          )
