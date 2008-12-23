c8b245f5b6bff8e88d8569883dc1697a6e9104f7
bourgon (Dr. ) <bourgon.richard@gene.com>
2008-12-23 13:33:44 +0000

    - Incremented revision number: 0.9.10.
    - Increased R dependency to 2.8.0.
    
    - Added check_valid argument to clusters methods, and included this argument in
      calls to reduce and interval_overlap calls in those methods. Updated
      documentation accordingly.
    
    - Added hyperref to vignette.
    
    - Added an example of the clusters method to the vignette.
    
    
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/intervals@225 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/clusters.R
+++ pkg/R/clusters.R
@@ -3,10 +3,10 @@ setGeneric( "clusters", function( x, ... ) standardGeneric( "clusters" ) )
 setMethod(
           "clusters",
           signature( "numeric" ),
-          function( x, w, which = FALSE ) {
+          function( x, w, which = FALSE, check_valid = TRUE ) {
             if ( is.integer(x) ) x <- as.numeric(x)
-            regions <- reduce( Intervals( cbind( x, x + w ), type = "R" ) )
-            clusters <- interval_overlap( regions, x )
+            regions <- reduce( Intervals( cbind( x, x + w ), type = "R" ), check_valid )
+            clusters <- interval_overlap( regions, x, check_valid )
             clusters <- clusters[ sapply( clusters, length ) > 1 ]
             if ( which ) return( clusters )
             else return( lapply( clusters, function(i) x[i] ) )
@@ -16,16 +16,17 @@ setMethod(
 setMethod(
           "clusters",
           signature( "Intervals_virtual" ),
-          function( x, w, which = FALSE ) {
+          function( x, w, which = FALSE, check_valid = TRUE ) {
             if ( type(x) == "Z" && ( w %% 1 != 0 ) )
               stop( "Non-integer 'w' supplied for intervals over Z.", call. = FALSE )
             regions <- reduce(
                               new(
                                   class(x),
                                   cbind( x[,1], x[,2] + w ), closed = closed(x), type = type(x)
-                                  )
+                                  ),
+                              check_valid
                               )
-            clusters <- interval_overlap( regions, x )
+            clusters <- interval_overlap( regions, x, check_valid )
             clusters <- clusters[ sapply( clusters, length ) > 1 ]
             if ( which ) return( clusters )
             else return( lapply( clusters, function(i) x[i,] ) )
